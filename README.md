c694bcf7ad9abc5f15e896cef5e05ea8cdf812c3
Yutsss <adyuta123@gmail.com>
2024-10-08 10:23:43 +0700

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# Tugas-Week-5-PBO
-Studi Kasus Music Organizer
+# **Studi Kasus Music Organizer**
+
+**Nama** : Adyuta Prajahita Murdianto
+
+**NRP** : 5025221186
+
+**Kelas** : PBO A
+
+## **Class Track**
+
+![Track](Resources/screenshot/Track.png)
+
+### **Fungsi**
+
+
+
+### **Atribut**
+
+
+
+### **Method**
+
+
+
+## **Class TrackReader**
+
+![TrackReader](Resources/screenshot/TrackReader.png)
+
+### **Fungsi**
+
+
+
+### **Atribut**
+
+
+
+### **Method**
+
+
+
+## **Class MusicPlayer**
+
+![MusicPlayer](Resources/screenshot/MusicPlayer.png)
+
+### **Fungsi**
+
+
+
+### **Atribut**
+
+
+
+### **Method**
+
+
+
+## **Class MainOrganizer**
+
+![MainOrganizer](Resources/screenshot/MainOrganizer.png)
+
+### **Fungsi**
+
+
+
+### **Atribut**
+
+
+
+### **Method**
+
---
+++ Resources/src/Music Organizer/src/MainOrganizer.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class MainOrganizer {
+    private ArrayList<Track> tracks;
+    private MusicPlayer player;
+    private TrackReader trackReader;
+
+    public MainOrganizer() {
+        tracks = new ArrayList<>();
+        player = new MusicPlayer();
+        trackReader = new TrackReader();
+    }
+
+    public void addTrack(String folderPath) {
+        tracks.addAll(trackReader.readTracks(folderPath));
+        System.out.println("Music library loaded. " + tracks.size() + " tracks.");
+    }
+
+    public void listAllTracks() {
+        if (tracks.isEmpty()) {
+            System.out.println("No tracks available.");
+        } else {
+            System.out.println("Track listing:");
+            for (int i = 0; i < tracks.size(); i++) {
+                System.out.println((i + 1) + ". " + tracks.get(i).getTitle());
+            }
+        }
+    }
+
+    public void playTrack(int index) {
+        if (index >= 0 && index < tracks.size()) {
+            Track track = tracks.get(index);
+            System.out.println("Playing file: " + track.getFilePath());
+            System.out.println("Now playing: " + track.getTitle());
+            player.play(track);
+        } else {
+            System.out.println("Invalid track index.");
+        }
+    }
+
+    public void stopPlaying() {
+        System.out.println("Stopping the current track.");
+        player.stop();
+    }
+
+    public static void main(String[] args) {
+        MainOrganizer organizer = new MainOrganizer();
+
+        organizer.addTrack("D:/Music/");
+
+        organizer.listAllTracks();
+
+        organizer.playTrack(0);
+
+        organizer.playTrack(1);
+
+        organizer.stopPlaying();
+
+        organizer.playTrack(2);
+    }
+}
